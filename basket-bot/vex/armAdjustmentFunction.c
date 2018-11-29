2a0eef928d146cdf634eb9f6f6826b83c8c77fb1
ndngo <ngonathanieldean@gmail.com>
2018-11-29 08:47:24 -0500

    reorganized file structure

---
+++ basket-bot/vex/armAdjustmentFunction.c
@@ -0,0 +1,24 @@
+#pragma config(Sensor, in2,    potentio,       sensorPotentiometer)
+
+//A global useful because the holding can be done in its own thread
+int isHolding = 0;
+int armThreshold = 0;//this will have to be determined with the robot present
+
+
+task adjustArm(){
+  while(1){
+    if(isHolding == 1 && SensorValue(petentio) < armThreshold){
+      motor[armMotor] = 20;
+    }
+  }
+}
+
+//make sure to set isHolding = 1 in the pickUp function
+isHolding = 1;
+
+//and make sure to set it to 0 in the put down function **before** actually putting it down
+isHolding = 0;
+
+//then call this in the main task
+
+startTask(adjustArm);
